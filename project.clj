e4228f572d816466ca65fd6ea31b58d3f4e0a927
oakes <zsoakes@gmail.com>
2016-04-19 10:01:46 -0400

    Increment version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mistakes-were-made "1.5.3-SNAPSHOT"
+(defproject mistakes-were-made "1.5.3"
   :description "An undo/redo system for Clojure and ClojureScript"
   :url "https://github.com/oakes/mistakes-were-made"
   :license {:name "Public Domain"
