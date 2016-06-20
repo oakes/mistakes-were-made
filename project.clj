8da655d2f0e3054564c3d74a9ceec1c7641077a0
oakes <zsoakes@gmail.com>
2016-06-20 14:10:03 -0400

    Start 1.6.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mistakes-were-made "1.6.4"
+(defproject mistakes-were-made "1.6.5-SNAPSHOT"
   :description "An undo/redo system for Clojure and ClojureScript"
   :url "https://github.com/oakes/mistakes-were-made"
   :license {:name "Public Domain"
